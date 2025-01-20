cc3c558f1e34f26fdf399c4e9826e86cd59d9ffd
Konstantin <litaistopet@iclou.com>
2025-01-20 23:09:31 +0800

    Добавлено: - Создано боковое меню для таблиц. - Отображение таблиц в виде таблиц.

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
-
+    path('employees/', views.employees_view, name='employees'),
+    path('positions/', views.positions_view, name='positions'),
+    path('production/', views.production_view, name='production'),
+    path('stages_production/', views.stages_production_view, name='stages_production'),
+    path('employee_position/', views.employee_position_view, name='employee_position'),
+    path('schedule/', views.schedule_view, name='schedule'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Production
+from .models import Employees, Positions, Production, StagesProduction, EmployeePosition, Schedule
 
 def home (request):
+    productions = Production.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +21,7 @@ def home (request):
             messages.warning(request,"Произошла ошибка, попробуйте еще раз")
             return redirect('home')
     else:
-        return render (request,'home.html')
+        return render (request,'home.html', {'productions': productions})
 
 def logout_user(request):
     logout(request)
@@ -41,3 +44,27 @@ def register_user(request):
         else:
             messages.error(request, "Произошла ошибка при регистрации.")
     return render(request, "register.html", {'form': form})
+
+def employees_view(request):
+    employees = Employees.objects.all()
+    return render(request, 'employees.html', {'employees': employees})
+
+def positions_view(request):
+    positions = Positions.objects.all()
+    return render(request, 'positions.html', {'positions': positions})
+
+def production_view(request):
+    production = Production.objects.all()
+    return render(request, 'production.html', {'production': production})
+
+def stages_production_view(request):
+    stages_production = StagesProduction.objects.all()
+    return render(request, 'stages_production.html', {'stages_production': stages_production})
+
+def employee_position_view(request):
+    employee_position = EmployeePosition.objects.all()
+    return render(request, 'employee_position.html', {'employee_position': employee_position})
+
+def schedule_view(request):
+    schedule = Schedule.objects.all()
+    return render(request, 'schedule.html', {'schedule': schedule})
